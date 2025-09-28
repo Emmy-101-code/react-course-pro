6cc40bd77a980758fd3978aec19d2e4ab715805a
Emmy-101-code <eokunola99@gmail.com>
2025-09-28 22:05:16 +0100

    Update HomePage.jsx
    adding render link

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -10,7 +10,7 @@ const[products, setProducts] = useState([]);
 
     useEffect(() => {
         const getHomeData = async () => {
-  const response = await axios.get('/api/products');
+  const response = await axios.get('https://react-course-ecommerce-backend-1.onrender.com/api/products');
        setProducts(response.data); 
         };
       
@@ -31,3 +31,4 @@ const[products, setProducts] = useState([]);
         </>
     );
 }
+
