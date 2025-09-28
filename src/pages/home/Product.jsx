673477a881997f9c041a1ace2b334ce3a4eec2cc
Emmy-101-code <eokunola99@gmail.com>
2025-09-28 22:14:55 +0100

    Update Product.jsx
    render link

--- src/pages/home/Product.jsx
+++ src/pages/home/Product.jsx
@@ -6,7 +6,7 @@ export function Product({product, loadCart}) {
     const [quantity, setQuantity] = useState(1);
 
     const addToCart = async () => {
-                    await axios.post('/api/cart-items', {
+                    await axios.post('https://react-course-ecommerce-backend-1.onrender.com/api/cart-items', {
                         productId: product.id,
                         quantity
                     });
@@ -78,3 +78,4 @@ export function Product({product, loadCart}) {
         </div>
     );
 }
+
